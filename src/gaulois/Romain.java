c9ff68e95fa2c578b389228643f267e28b74a7a9
Loukas Gbedo <gbl5242a@ensfsi.onmicrosoft.com>
2025-03-18 17:44:24 +0100

    Invariant / Pre/Post Condition

--- src/gaulois/Romain.java
+++ src/gaulois/Romain.java
@@ -7,6 +7,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert this.isInvariantVerified();
 	}
 
 	public String getNom() {
@@ -18,19 +19,35 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert forceCoup >= 0;
+		int forcedebut = force;
+				
 		force = force - forceCoup;
 		if (force  < 1) {
 			parler("J'abandonne !");
 		}
 		else {
 			parler("Aie !");
-			
+		
 		}
+		assert this.isInvariantVerified();
+		assert forcedebut < force;
 		
 	}
 
+	private Boolean isInvariantVerified() {
+		return this.force >= 0;
+	}
+	
 	
 	private String prendreParole( ) {
 		return "Le romain " + nom + " : ";
+	}
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", -6);
+		
+		minus.isInvariantVerified();
+		
 	}
 }
